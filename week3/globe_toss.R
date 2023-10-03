50bc9d2766edd7c9181146b87566ca984476ad12
erik-ringen <erik-ringen@users.noreply.github.com>
2023-10-03 15:05:20 +0200

    class prep function

--- week3/globe_toss.R
+++ week3/globe_toss.R
@@ -8,6 +8,11 @@ globe_toss <- function(prior_alpha = 1, prior_beta = 1, W, L) {
   mtext(paste("W =", W, " L =", L))
 }
 
-# update the values of W and L according to the observed data
-globe_toss(W = 9, L = 2)
+# Record the results here
+results <- c()
 
+# update the values of W and L according to the observed data
+globe_toss(W = 1, L = 10)
+
+# What if we tried with a non-flat prior?
+globe_toss(prior_alpha = 4, prior_beta = 2, W = 1, L = 10)
