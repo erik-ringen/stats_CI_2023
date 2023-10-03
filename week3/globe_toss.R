7d332b8aa97084c8b79b1e1c5bf1ba9343c9762b
erik-ringen <erik-ringen@users.noreply.github.com>
2023-10-03 15:26:49 +0200

    default counts to 0

--- week3/globe_toss.R
+++ week3/globe_toss.R
@@ -12,7 +12,7 @@ globe_toss <- function(prior_alpha = 1, prior_beta = 1, W, L) {
 results <- c()
 
 # update the values of W and L according to the observed data
-globe_toss(W = 1, L = 10)
+globe_toss(W = 0, L = 0)
 
 # What if we tried with a non-flat prior?
 globe_toss(prior_alpha = 4, prior_beta = 2, W = 1, L = 10)
